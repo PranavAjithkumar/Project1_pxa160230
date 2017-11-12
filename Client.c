1cc611e67466d8a8e845cc4ecf768804dce4e660
PranavAjithkumar <33589482+PranavAjithkumar@users.noreply.github.com>
2017-11-11 21:46:04 -0600

    Add files via upload

--- Client.c
+++ Client.c
@@ -11,48 +11,60 @@
 #define SERV_PORT 10010 /*port*/
 
 int
-main(int argc, char **argv) 
+main(int argc, char **argv)
 {
  int sockfd;
  struct sockaddr_in servaddr;
  char sendline[MAXLINE], recvline[MAXLINE];
 
  // alarm(300);  // to terminate after 300 seconds
-	
+
  //basic check of the arguments
  //additional checks can be inserted
- if (argc !=2) {
-  perror("Usage: TCPClient <Server IP> <Server Port>"); 
+
+//new line added to print the number of arguments passed
+ printf("Number of Arguments Passed    : %d\n",argc);
+
+ //Changed from 2 to 3
+ if (argc !=3)
+ {
+  perror("Usage: TCPClient <Server IP> <Server Port>");
   exit(1);
- }
-	
+}
+
+//if successful, print the IP Address and Port Number
+ printf("Server IP Address  : %s\n", argv[1]);
+ printf("Server Port Number : %s\n", argv[2]);
+
  //Create a socket for the client
  //If sockfd<0 there was an error in the creation of the socket
  if ((sockfd = socket (AF_INET, SOCK_STREAM, 0)) <0) {
   perror("Problem in creating the socket");
   exit(2);
  }
-	
+
+ //Overwrite the SERV_PORT with passed argument port number
  //Creation of the socket
  memset(&servaddr, 0, sizeof(servaddr));
  servaddr.sin_family = AF_INET;
  servaddr.sin_addr.s_addr= inet_addr(argv[1]);
- // servaddr.sin_port =  htons(argv[2]); 
- servaddr.sin_port =  htons(SERV_PORT); //convert to big-endian order
-	
- //Connection of the client to the socket 
+ servaddr.sin_port =  htons((int) strtol(argv[2], (char **)NULL, 10));
+ //servaddr.sin_port =  htons(SERV_PORT); //convert to big-endian order
+
+
+ //Connection of the client to the socket
  if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr))<0) {
   perror("Problem in connecting to the server");
   exit(3);
  }
-	
+
  while (fgets(sendline, MAXLINE, stdin) != NULL) {
-	
+
   send(sockfd, sendline, strlen(sendline), 0);
-		
+
   if (recv(sockfd, recvline, MAXLINE,0) == 0){
    //error: server terminated prematurely
-   perror("The server terminated prematurely"); 
+   perror("The server terminated prematurely");
    exit(4);
   }
   printf("%s", "String received from the server: ");
